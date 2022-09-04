8de01cbc298a9eee6ef19c46448aedb0a42be1dc
DongYunLee372 <dlehdgus1089@naver.com>
2022-09-04 20:28:09 +0900

    버튼리스너 생성 및 씬수정

--- Assets/Scripts/UI/ButtonListner.cs
+++ Assets/Scripts/UI/ButtonListner.cs
@@ -5,21 +5,37 @@ using UnityEngine.UI;
 
 public class ButtonListner : MonoBehaviour
 {
-    public Button button;
+    public Button B_GameStart;
+    public Button B_Option;
+    public Button B_Restart;
+    public Button B_Exitgame;
     // Start is called before the first frame update
     void Start()
     {
-        button.onClick.AddListener(GameStart);
+        B_GameStart.onClick.AddListener(GameStart);
+        B_Option.onClick.AddListener(Option);
+        B_Restart.onClick.AddListener(Restart);
+        B_Exitgame.onClick.AddListener(Exitgame);
+
     }
 
     // Update is called once per frame
-    void Update()
+    void Exitgame()
     {
-        
+
+    }
+    void Restart()
+    {
+
     }
 
+    void Option()
+    {
+
+    }
     void GameStart()
     {
         Debug.Log("gd");
     }
+  
 }
