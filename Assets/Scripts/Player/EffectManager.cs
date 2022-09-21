a20101cecb0e21dba3f0066b9d31c2bd7c8998ab
JO <kio7147@naver.com>
2022-09-22 00:17:04 +0900

    이펙트매니저 수정

--- Assets/Scripts/Player/EffectManager.cs
+++ Assets/Scripts/Player/EffectManager.cs
@@ -5,7 +5,16 @@ using UnityEngine;
 
 //생성을 원하는 이펙트와 위치, 생성되어 있을 시간을 넘겨주면 해당 이펙트를 생성해준다.
 //이펙트의 부모를 설정해주면 이펙트가 따라다니도록
-//리소스의 모든 이펙트들을 받아와서 가지고 있는다
+//리소스의 모든 이펙트들을 받아와서 가지고 있는다 => 어드레서블로 로드 가능하도록
+
+
+//2022.09.22 까지 만들 것
+//첫번쨰 루프만 실행하고 사라지는 기능
+//루프설정 가능하도록
+//한번에 같은 소스를 가지고 있는 이펙트가 2개 이상 씬에 생기면 해당 이펙트는 오브젝트 풀에 넣어서 관리한다. 
+//이펙트의 모든 파티클들의 duration을 조사해서 해당 duration이 끝나면 삭제되ㅗ도록
+//일정 주기로 다시실행하도록(비활성화시켰다가 활성화?)
+
 
 public class EffectManager : MySingleton<EffectManager>
 {
@@ -19,6 +28,9 @@ public class EffectManager : MySingleton<EffectManager>
     public CorTimeCounter timer = new CorTimeCounter();
 
     public IEnumerator cor;
+
+
+
 
     public GameObject InstantiateEffect(GameObject effect)
     {
@@ -121,10 +133,9 @@ public class EffectManager : MySingleton<EffectManager>
         }
     }
 
-    //// Start is called before the first frame update
-    //void Start()
-    //{
-    //    BaseEffect = new GameObject("Effects").transform;
-    //}
+    public void SetLoop(ParticleSystem effect, bool b)
+    {
+
+    }
 
 }
