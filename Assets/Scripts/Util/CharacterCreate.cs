1776dc70ef6caf5da796d6645cb1468b5ee19598
DongYunLee372 <dlehdgus1089@naver.com>
2022-09-23 20:25:54 +0900

    보스생성부분 수정

--- Assets/Scripts/Util/CharacterCreate.cs
+++ Assets/Scripts/Util/CharacterCreate.cs
@@ -68,7 +68,7 @@ public class CharacterCreate : Singleton<CharacterCreate>
 
         List<BossNomalSkill> bossNomalSkills = new List<BossNomalSkill>();
         List<Mon_Normal_Atk_Group> mon_Normal_Atk_Group = new List<Mon_Normal_Atk_Group>();
-        MonsterSkillInformation monsterSkillInformation = new MonsterSkillInformation();
+        MonsterSkillInformation monsterSkillInformation = ScriptableObject.CreateInstance<MonsterSkillInformation>();
 
         bossNomalSkills.Add(DataLoad_Save.Instance.Get_BossSkillDB(Global_Variable.Boss.USwing));
         bossNomalSkills.Add(DataLoad_Save.Instance.Get_BossSkillDB(Global_Variable.Boss.SSwing));
