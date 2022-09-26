aa8f83e5852f8225568adbc88ff765cd281cdbf5
browniess <abcdkl45678@gmail.com>
2022-09-26 21:53:31 +0900

    123

--- Assets/Scripts/Util/Battle_Character.cs
+++ Assets/Scripts/Util/Battle_Character.cs
@@ -438,7 +438,7 @@ public class Battle_Character : MonoBehaviour
         real_AI.navMesh.speed = speed;
         real_AI.navMesh.acceleration = acc;
 
-        yield return new WaitForSeconds(1.0f);
+        yield return new WaitForSeconds(1.5f);
 
         real_AI.navMesh.angularSpeed = 120f;
     }
