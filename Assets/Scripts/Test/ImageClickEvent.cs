29c36980c99aeed1b91fec3c2a8ff210c5b8a3cb
[Github <[Github>
2022-10-23 00:33:02 +0900

    이미지

---
+++ Assets/Scripts/Test/ImageClickEvent.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+public class ImageClickEvent : MonoBehaviour//, IPointerClickHandler
+    //, IDragHandler
+    //, IPointerEnterHandler
+    //, IPointerExitHandler
+{
+
+    //public void OnPointerClick(PointerEventData eventData)
+    //{
+    //    Debug.Log("히히Click");
+    //}
+
+   
+    void OnMouseDown()
+    {
+            Debug.Log("히히Click");
+
+    }
+
+
+}
