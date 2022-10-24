a83118cf57d908357f6f5ca35318eed8e0b3c5b6
[Github <[Github>
2022-10-24 20:00:40 +0900

    로딩수정

--- Assets/Scripts/Test/GameData_Load.cs
+++ Assets/Scripts/Test/GameData_Load.cs
@@ -337,6 +337,8 @@ public class GameData_Load :Singleton<GameData_Load>
         var temp=AddressablesLoadManager.Instance.Find_InstantiateObj<GameObject>("PlayerCharacter");
         AddressablesLoadManager.Instance.Delete_Object<GameObject>(temp);  //캐릭터 삭제. 
                                                                            //몬스터 추가되면 삭제
+        var temp1 = AddressablesLoadManager.Instance.Find_InstantiateObj<GameObject>("Skeleton");
+        AddressablesLoadManager.Instance.Delete_Object<GameObject>(temp1);  //캐릭터 삭제. 
 
         LoadingImageShow(Scenes_Stage.Stage1);  //로딩 이미지
     }
