40883e4129461441ef414046468a561c143a4c9c
Chae <asd>
2022-10-25 17:36:37 +0900

    456

--- Assets/Scripts/Cinema_Cam.cs
+++ Assets/Scripts/Cinema_Cam.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class Cinema_Cam : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Camera cam;
+    public float PosZ = 0f;
+    Vector3 velo = Vector3.zero;
+    Vector3 v;
+    float x;
+    public float val = 0.001f;
+    
     void Start()
     {
+        cam = GetComponent<Camera>();
+        v = cam.transform.position;
+        v.z += 20f;
+        CamStart();
+    }
+    void CamStart()
+    {
+        StartCoroutine(MoveCam());
+        PlayableCharacter.Instance.gameObject.SetActive(false);
+    }
+    IEnumerator MoveCam()
+    {
+        x = Time.time;
         
+        while (true)
+        {
+           
+            cam.transform.position = Vector3.SmoothDamp(cam.transform.position, v, ref velo, val);
+
+
+            if (Time.time - x >= 4f)
+            {
+                cam.gameObject.SetActive(false);
+                break;
+            }
+
+            yield return null;
+        }
+
+        PlayableCharacter.Instance.gameObject.SetActive(true);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        //cam.transform.position = Vector3.SmoothDamp(cam.transform.position, v, ref velo, 0.1f);
     }
 }
