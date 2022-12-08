fa613f2a0a46a4993d8498cddb2e8ce3a68b21d2
[Github <[Github>
2022-12-08 23:49:47 +0900

    dd

--- Assets/Scripts/Util/Portal_stage.cs
+++ Assets/Scripts/Util/Portal_stage.cs
@@ -5,12 +5,16 @@ using UnityEngine;
 public class Portal_stage : MonoBehaviour
 {
     // Start is called before the first frame update
-
+    bool flag = false;
     private void OnTriggerEnter(Collider other)
     {
         if(other.tag=="Player")
         {
-            GameData_Load.Instance.ChangeScene(Scenes_Stage.Stage1);
+            if(!flag)
+            {
+                GameData_Load.Instance.ChangeScene(Scenes_Stage.Stage1);
+                flag = true;
+            }
         }
     }
 }
