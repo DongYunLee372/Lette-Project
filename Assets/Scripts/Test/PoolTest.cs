d01fcf57a7be218d5f03f84661c2942e3390c95d
[Github <[Github>
2022-12-09 19:27:58 +0900

    aa

--- Assets/Scripts/Test/PoolTest.cs
+++ Assets/Scripts/Test/PoolTest.cs
@@ -7,9 +7,32 @@ public class PoolTest : MonoBehaviour
    
     void Start()
     {
-       GameObject temp=  GameMG.Instance.Resource.Instantiate<GameObject>("Skeleton_Warrior");
 
-        GameMG.Instance.Resource.Destroy<GameObject>(temp);
+        GameObject temp4 = GameMG.Instance.Resource.Instantiate<GameObject>("WeaponCol");
+
+        GameObject temp1 =  GameMG.Instance.Resource.Instantiate<GameObject>("Skeleton_Warrior");
+
+        //  GameMG.Instance.Resource.Destroy<GameObject>(temp1);
+    
+       // GameObject temp2 = GameMG.Instance.Resource.Instantiate<Object.Colliders>("SphereColl");
+     //   GameObject temp3 =  GameMG.Instance.Resource.Instantiate<Colliders>("BoxColl");
+
+
+
+        StartCoroutine(temp());
+    }
+
+    IEnumerator temp()
+    {
+        int count = 10;
+        while(count>=0)
+        {
+            yield return new WaitForSeconds(1f);
+            GameObject temp = GameMG.Instance.Resource.Instantiate<GameObject>("Skeleton_Warrior");
+            count--;
+        }
+       
+
     }
 
     // Update is called once per frame
