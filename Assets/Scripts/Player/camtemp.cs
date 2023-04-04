cc653884035ce0b81511d95a5ba49521b35836a6
JO <kio7147@naver.com>
2023-04-04 21:11:52 +0900

    테스트

--- Assets/Scripts/Player/camtemp.cs
+++ Assets/Scripts/Player/camtemp.cs
@@ -2,8 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class camtemp : MonoBehaviour
+public abstract class camtemp : MonoBehaviour
 {
+
+    public abstract void teest();
+
     // Start is called before the first frame update
     void Start()
     {
